8a4a4b2664af9f7173a6ae15a3c3195a121319d3
web-raketa <39305243+web-raketa@users.noreply.github.com>
2018-09-26 17:47:38 +0300

    tinyone-2
    готов

--- homeWork_4/myproject_2/app/js/common.js
+++ homeWork_4/myproject_2/app/js/common.js
@@ -1,3 +1,22 @@
-$(function () {
-    
-})
+var openMenu = document.querySelector(".button_menu");
+var menuForm = document.querySelector(".main-menu_list");
+var closeMenu = document.querySelector(".menu_close");
+
+openMenu.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    menuForm.classList.add("modal-show");
+});
+
+// closeMenu.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     menuForm.classList.remove("modal-show");
+// })
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (menuForm.classList.contains("modal-show")) {
+            menuForm.classList.remove("modal-show");
+        }
+    }
+});
