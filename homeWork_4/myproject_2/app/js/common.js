a9862e8ae929e5b116cf6a26148ddcd8d113a154
web-raketa <39305243+web-raketa@users.noreply.github.com>
2018-09-26 19:53:09 +0300

    изменил js
    теперь меню откр и закр по клику, по esc не закрывается )
    
    вариант 2 придумал не сам
    по js вооще не сам написал, 1вар это академия 2 вар видел в коде

--- homeWork_4/myproject_2/app/js/common.js
+++ homeWork_4/myproject_2/app/js/common.js
@@ -1,22 +1,36 @@
-var openMenu = document.querySelector(".button_menu");
-var menuForm = document.querySelector(".main-menu_list");
-var closeMenu = document.querySelector(".menu_close");
+// вариант 1
+// var openMenu = document.querySelector(".button_menu");
+// var menuForm = document.querySelector(".main-menu_list");
+// var closeMenu = document.querySelector(".menu_close");
 
-openMenu.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    menuForm.classList.add("modal-show");
-});
+// openMenu.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     menuForm.classList.add("modal-show");
+// });
 
 // closeMenu.addEventListener("click", function (evt) {
 //     evt.preventDefault();
 //     menuForm.classList.remove("modal-show");
 // })
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (menuForm.classList.contains("modal-show")) {
-            menuForm.classList.remove("modal-show");
-        }
-    }
-});
+// window.addEventListener("keydown", function (evt) {
+//     if (evt.keyCode === 27) {
+//         evt.preventDefault();
+//         if (menuForm.classList.contains("modal-show")) {
+//             menuForm.classList.remove("modal-show");
+//         }
+//     }
+// });
+
+// вариант 2
+
+function menu_clikc() {
+	var menu = document.querySelector(".page-header__main-menu");
+
+	if (menu.style.visibility == "visible"){
+		menu.style.visibility = "hidden";
+	}
+	else {
+		menu.style.visibility = "visible";
+	}
+}
